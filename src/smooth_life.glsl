96395e46d9713ec1d0246bf624c5d868240ee8e1
Ahmed Hassayoune <ahmed.hassayoune@epita.fr>
2024-07-11 23:03:29 +0200

    fix: finished with realistic result

--- src/smooth_life.glsl
+++ src/smooth_life.glsl
@@ -13,40 +13,37 @@ const int KERNEL_SIZE = int(OUTER_RADIUS);
 const float INNER_AREA = PI * INNER_RADIUS * INNER_RADIUS;
 const float ANNULUS_AREA = PI * (OUTER_RADIUS * OUTER_RADIUS - INNER_RADIUS * INNER_RADIUS);
 
-const float B1 = 0.278;
-const float B2 = 0.365;
+const float B1 = 0.257;
+const float B2 = 0.335;
 
-const float D1 = 0.267;
-const float D2 = 0.445;
+const float D1 = 0.365;
+const float D2 = 0.549;
 
 const float ALPHA_N = 0.028;
 const float ALPHA_M = 0.147;
 
-const float dt = 0.3;
+const float dt = 0.4;
 
-float sigma(float x, float a, float alpha) {
+float sigmoid(float x, float a, float alpha) {
     return 1.0 / (1.0 + exp(-4.0 * (x - a) / alpha));
 }
 
 float smooth_edge(float x, float cutoff) {
-    return 1.0 - sigma(x, cutoff, 0.5);
+    return 1.0 - sigmoid(x, cutoff, 0.5);
 }
 
-float sigma_m(float m, float x, float y) {
-    float w = sigma(m, 0.5, ALPHA_M);
-    return x * (1.0 - w) + y * w;
+float sigmoid_ab(float x, float a, float b) {
+    return sigmoid(x, a, ALPHA_N) * (1.0 - sigmoid(x, b, ALPHA_N));
 }
 
-float sigma_n(float x, float a, float b) {
-    return sigma(x, a, ALPHA_N) * (1.0 - sigma(x, b, ALPHA_N));
+float sigmoid_mix(float x, float y, float m) {
+    return x * (1.0 - sigmoid(m, 0.5, ALPHA_M)) + y * sigmoid(m, 0.5, ALPHA_M);
 }
 
-float S(float m, float n) {
-    return sigma_n(
-        n,
-        sigma_m(m, B1, D1),
-        sigma_m(m, B2, D2)
-    );
+float S(float n, float m) {
+    float thresh_b = sigmoid_ab(n, B1, B2);
+    float thresh_d = sigmoid_ab(n, D1, D2);
+    return sigmoid_mix(thresh_b, thresh_d, m);
 }
 
 vec2 integrate(vec2 uv) {
@@ -87,10 +84,10 @@ void main(void) {
         return;
     }
 
-    vec3 color;
-    color = texture(iChannel0, uv).rgb;
     vec2 mn = integrate(uv);
-    float s = S(mn.x, mn.y);
+    float s = S(mn.y, mn.x);
+
+    vec3 color = texture(iChannel0, uv).rgb;
     color.r = color.r + dt * (2.0 * s - 1.0);
     color.gb = mn;
     color = clamp(color, 0.0, 1.0);
@@ -98,7 +95,8 @@ void main(void) {
     if(iMouse.z > 0.0) {
         float d = length(gl_FragCoord.xy - iMouse.xy);
         if(d <= OUTER_RADIUS) {
-            color.x = sigma(d, INNER_RADIUS, 0.5);
+            float val = sigmoid(d, INNER_RADIUS, 0.5);
+            color = id(val).rgb;
         }
     }
 
